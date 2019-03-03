03bbef459340d8dd34b7b2e7b4254498af04b4df
lsy <371339601@qq.com>
2019-03-04 04:59:10 +0800

    次

---
+++ mvcCommon/src/main/java/com/lsy/common/UserService.java
@@ -0,0 +1,5 @@
+package com.lsy.common;
+
+public interface UserService {
+	public String selectList();
+}
---
+++ mvcParent/mvcService/src/main/java/com/cssweb/yhzqjj/user/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.cssweb.yhzqjj.user.serviceImpl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.lsy.common.UserService;
+@Service
+public class UserServiceImpl implements UserService {
+	public String selectList() {
+		return "ss11";
+	}
+
+}
